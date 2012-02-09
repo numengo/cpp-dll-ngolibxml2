92ab1a898dd15c1db3d4bcef6c75634b773c41ce
Cedric <roman@numengo.com>
2012-02-09 23:29:10 +0100

    improved library versionning for non windows platform and management in numengo library manager removed useless scripts for build
    Signed-off-by: Cedric <roman@numengo.com>

---
+++ version.h
@@ -0,0 +1,16 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define RC_FILENAME "NgoLibxml2\0"
+#define RC_DESCRIPTION_STRING "numenGo version of libxml2\0"
+#define RC_FILEVERSION 2,7,2,0
+#define RC_FILEVERSION_STRING "2.7rev2\0"
+
+#define RC_PRODUCTNAME_STRING "numenGo|Toolbox\0"
+#define RC_PRODUCTVERSION 12,02,0,0
+#define RC_PRODUCTVERSION_STRING "12.02rev0\0"
+
+#define RC_COMPANYNAME "NUMENGO\0"
+#define RC_COPYRIGHT_STRING "Daniel Veillard\0"
+
+#endif //VERSION_H
