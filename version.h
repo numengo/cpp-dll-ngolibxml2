c63108e015e67625e3b36f4a61b9e2bfd100fe85
Cedric ROMAN <roman@numengo.com>
2013-12-06 11:57:20 +0100

    - update version
    Signed-off-by: Cedric ROMAN <roman@numengo.com>

--- version.h
+++ version.h
@@ -3,12 +3,12 @@
 
 #define RC_FILENAME "NgoLibxml2\0"
 #define RC_DESCRIPTION_STRING "numenGo version of libxml2\0"
-#define RC_FILEVERSION 2,7,2,0
-#define RC_FILEVERSION_STRING "2.7rev2\0"
+#define RC_FILEVERSION 2,8,2,0
+#define RC_FILEVERSION_STRING "2.8rev2\0"
 
 #define RC_PRODUCTNAME_STRING "numenGo|Toolbox\0"
-#define RC_PRODUCTVERSION 12,02,0,0
-#define RC_PRODUCTVERSION_STRING "12.02rev0\0"
+#define RC_PRODUCTVERSION 13,12,1,0
+#define RC_PRODUCTVERSION_STRING "13.12rev1\0"
 
 #define RC_COMPANYNAME "NUMENGO\0"
 #define RC_COPYRIGHT_STRING "Daniel Veillard\0"
